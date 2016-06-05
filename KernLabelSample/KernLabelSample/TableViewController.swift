bdb3f16d1357bc8bdcd2de2d49c848949b34d51c
Taishi Ikai <t@ikai.tokyo>
2016-06-05 22:45:39 +0900

    Improve performance of sample table

--- KernLabelSample/KernLabelSample/TableViewController.swift
+++ KernLabelSample/KernLabelSample/TableViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 import KernLabel
 
+
 class TableViewController: UITableViewController {
 
-    var paragraphStyle: NSParagraphStyle {
-        let style = NSMutableParagraphStyle()
-        style.lineHeightMultiple = 1.2
-        return style
-    }
+    var heights: [NSIndexPath: CGFloat] = [:]
+    var images: [NSIndexPath: CGImage?] = [:]
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -34,8 +32,6 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
         self.tableView.registerClass(Cell.self, forCellReuseIdentifier: "Cell")
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
-        self.tableView.rowHeight = UITableViewAutomaticDimension
-        self.tableView.estimatedRowHeight = 100
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -51,40 +47,50 @@ class TableViewController: UITableViewController {
         return Sentences.count
     }
 
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if let height = self.heights[indexPath] {
+            return height
+        }
+        let str = KernTypeString(string: Sentences[indexPath.row], attributes: [
+            NSParagraphStyleAttributeName: Cell.paragraphStyle,
+            NSFontAttributeName: Cell.font
+        ])
+        let height = str.boundingHeight(tableView.frame.width - 30, options: [], numberOfLines: 0, context: nil) + 30
+        self.heights[indexPath] = height
+        let image = str.createImage(CGRectMake(0, 0, tableView.frame.width, height - 30), options: [])
+        self.images[indexPath] = image
+        return height
+    }
+
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    }
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! Cell
-        cell.label?.numberOfLines = 0
-        cell.label?.attributedText = NSAttributedString(
-            string: Sentences[indexPath.row],
-            attributes: [
-                NSParagraphStyleAttributeName: self.paragraphStyle,
-                NSFontAttributeName: UIFont.systemFontOfSize(17)
-            ]
-        )
+        cell.titleLayer.frame = CGRectMake(15, 15, cell.frame.width - 30, cell.frame.height - 30)
+        cell.titleLayer.contents = self.images[indexPath]!
         return cell
     }
 }
 
 private class Cell: UITableViewCell {
-    var label: KernLabel?
+    var type: KernTypeString!
     var indexPath: NSIndexPath?
+
+    var titleLayer = CALayer()
+
+    static var paragraphStyle: NSParagraphStyle {
+        let style = NSMutableParagraphStyle()
+        style.lineHeightMultiple = 1.2
+        return style
+    }
+    static let font = UIFont.systemFontOfSize(24)
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.label = KernLabel(frame: CGRectZero)
-        self.label?.removeFromSuperview()
-        self.label?.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addSubview(self.label!)
-        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-10-[view]-10-|",
-            options: [],
-            metrics: nil,
-            views: ["view": self.label!]))
-        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-15-[view]-15-|",
-            options: [],
-            metrics: nil,
-            views: ["view": self.label!]))
+        self.backgroundColor = UIColor.whiteColor()
+        self.opaque = true
+        self.contentView.layer.addSublayer(self.titleLayer)
     }
 
     required init?(coder aDecoder: NSCoder) {
