9d14469d7fb119edf6f0098625063a4c94017bfd
Taishi Ikai <t@ikai.tokyo>
2016-06-15 22:00:56 +0900

    Modify ViewTableViewController sample to use UIView instead of CALayer on cells

--- KernLabelSample/KernLabelSample/ViewTableViewController.swift
+++ KernLabelSample/KernLabelSample/ViewTableViewController.swift
@@ -47,31 +47,30 @@ class ViewTableViewController: UITableViewController {
         if let height = self.heights[indexPath] {
             return height
         }
-        let type = KernTypeString(string: Sentences[indexPath.row % Sentences.count], attributes: [
-            NSParagraphStyleAttributeName: ViewTableCellLayer.paragraphStyle,
-            NSFontAttributeName: ViewTableCellLayer.font
-        ])
-        let height = type.boundingHeight(tableView.frame.width - 30, options: [], numberOfLines: 0, context: nil) + 30
+        let type = KernTypeString(
+            string: Sentences[indexPath.row % Sentences.count], attributes: TitleView.attributes)
+        let height = type.boundingHeight(
+            tableView.frame.width - 30, options: [], numberOfLines: 0, context: nil) + 30
         self.heights[indexPath] = height
         return height
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ViewTableCell", forIndexPath: indexPath) as! ViewTableCell
-        cell.titleLayer.type.string = Sentences[indexPath.row % Sentences.count]
-        cell.titleLayer.frame = CGRectMake(15, 15, cell.frame.width - 30, cell.frame.height - 30)
-        cell.titleLayer.setNeedsDisplay()
+        cell.titleView.text = Sentences[indexPath.row % Sentences.count]
+        cell.titleView.frame = CGRectMake(15, 15, cell.frame.width - 30, cell.frame.height - 30)
+        cell.titleView.setNeedsDisplay()
         return cell
     }
 }
 
 
 private class ViewTableCell: UITableViewCell {
-    var titleLayer = ViewTableCellLayer()
+    var titleView = TitleView(frame: CGRectZero)
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.contentView.layer.addSublayer(self.titleLayer)
+        self.contentView.addSubview(self.titleView)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -79,36 +78,33 @@ private class ViewTableCell: UITableViewCell {
     }
 }
 
-private class ViewTableCellLayer: CALayer {
-    var type: KernTypeString = KernTypeString(string: "")
+private class TitleView: UIView {
+    var text = ""
 
-    static var paragraphStyle: NSParagraphStyle {
-        let style = NSMutableParagraphStyle()
-        style.lineHeightMultiple = 1.2
-        return style
-    }
-    static let font = UIFont.systemFontOfSize(24)
-
-    override init() {
-        super.init()
-        self.delegate = self
-        self.contentsScale = UIScreen.mainScreen().scale
-        self.type.attributes = [
-            NSParagraphStyleAttributeName: ViewTableCellLayer.paragraphStyle,
-            NSFontAttributeName: ViewTableCellLayer.font
+    static var attributes: [String : AnyObject] {
+        var paragraphStyle: NSParagraphStyle {
+            let style = NSMutableParagraphStyle()
+            style.lineHeightMultiple = 1.2
+            return style
+        }
+        return [
+            NSParagraphStyleAttributeName: paragraphStyle,
+            NSFontAttributeName: UIFont.systemFontOfSize(24)
         ]
     }
 
-    override init(layer: AnyObject) {
-        super.init(layer: layer)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clearColor()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
-        self.type.drawWithRect(layer.bounds, options: .UsesLineFragmentOrigin, context: ctx)
+    private override func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        let type = KernTypeString(string: self.text, attributes: self.dynamicType.attributes)
+        type.drawWithRect(rect, options: .UsesLineFragmentOrigin, context: context)
     }
 }
-
