b110bac32912a563206210d7ec5a17c5e13c5dcf
Taishi Ikai <t@ikai.tokyo>
2016-07-03 23:11:47 +0900

    Add TabBarChild and TableViewController classes

---
+++ KernLabelSample/KernLabelSample/TabBarChild.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarChild.swift
+//  KernLabelSample
+//
+//  Created by Taishi Ikai on 2016/07/03.
+//  Copyright © 2016年 Taishi Ikai. All rights reserved.
+//
+
+import UIKit
+
+protocol TabBarChild: class {
+}
+
+extension TabBarChild where Self: UIViewController {
+    func setNavigationBar(title: String? = nil,
+                          leftBarButtonItems:[UIBarButtonItem]? = nil,
+                          rightBarButtonItems: [UIBarButtonItem]? = nil) {
+        self.tabBarController?.navigationItem.leftBarButtonItems = leftBarButtonItems
+        self.tabBarController?.navigationItem.rightBarButtonItems = rightBarButtonItems
+        self.tabBarController?.navigationItem.title = title ?? self.title
+    }
+}
---
+++ KernLabelSample/KernLabelSample/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  KernLabelSample
+//
+//  Created by Taishi Ikai on 2016/07/03.
+//  Copyright © 2016年 Taishi Ikai. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController, TabBarChild {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setNavigationBar()
+        self.tableView.contentInset = UIEdgeInsetsMake(self.topLayoutGuide.length, 0, self.bottomLayoutGuide.length, 0)
+    }
+}
